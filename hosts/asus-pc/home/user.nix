cc763d4de01587287601e4d992494a26963acf2c
griffi-gh <prasol258@gmail.com>
2024-07-24 16:20:20 +0200

    configure git

--- hosts/asus-pc/home/user.nix
+++ hosts/asus-pc/home/user.nix
@@ -11,13 +11,28 @@
   programs = {
     home-manager.enable = true;
 
-    fish.enable = true;
+    fish = {
+      enable = true;
+      shellAliases = {
+        cat = "bat";
+        cls = "clear";
+        "cd.." = "cd ..";
+      };
+    };
     nushell.enable = true;
     carapace.enable = true;
     starship = {
       enable = true;
       enableTransience = true;
     };
+    bat.enable = true;
+
+    git = {
+      enable = true;
+      userName = "griffi-gh";
+      userEmail = "prasol258@gmail.com";
+      lfs.enable = true;
+    }
   };
 
   imports = [
