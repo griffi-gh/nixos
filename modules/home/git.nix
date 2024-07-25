ee3d48c5fd480f2378561bbb8547a4c48c86ead5
griffi-gh <prasol258@gmail.com>
2024-07-25 14:34:05 +0200

    use cred helper

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,13 +1,14 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.git = {
     enable = true;
+    package = pkgs.gitFull;
     userName = "griffi-gh";
     userEmail = "prasol258@gmail.com";
     lfs.enable = true;
     extraConfig = {
       init.defaultBranch = "master";
-      credential.helper = "store";
       core.editor = "nano";
+      credential.helper = "${pkgs.gitFull}/bin/git-credential-libsecret";
     };
   };
 }
