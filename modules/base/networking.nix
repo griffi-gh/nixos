36276d81755fc99a335923f7b8f7e446e4b20acd
griffi-gh <prasol258@gmail.com>
2024-07-25 13:14:11 +0200

    forward zt port

--- modules/base/networking.nix
+++ modules/base/networking.nix
@@ -13,13 +13,14 @@
       allowedTCPPorts = [
         22    # ssh
         3389  # rdp
-        54686 # vscode-server port forwarding
+        9993  # zerotier
       ];
       allowedUDPPorts = [
         3389  # rdp
+        9993  # zerotier
       ];
     };
-    
+
     # proxy.default = "http://user:password@proxy:port/";
     # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
   };
--- modules/base/services/zerotier.nix
+++ modules/base/services/zerotier.nix
@@ -1,6 +1,7 @@
 { config, lib, pkgs, ... }: {
   services.zerotierone = {
     enable = true;
+    port = 9993;
     joinNetworks = [
       "9e1948db635b9d3e" # personal
     ];
