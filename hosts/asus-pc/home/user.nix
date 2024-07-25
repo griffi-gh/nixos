24703bc4e7ee7bee7c20eb0e92cd8e2e22c07fa0
griffi-gh <prasol258@gmail.com>
2024-07-25 15:31:14 +0200

    add stuff to user.nix

--- hosts/asus-pc/home/user.nix
+++ hosts/asus-pc/home/user.nix
@@ -4,16 +4,23 @@
     username = "user";
     homeDirectory = "/home/user";
     packages = with pkgs; [
+      # CLI tools:
       htop
       tree
       glxinfo
-      nushell
-      firefox
-      thunderbird
-      libreoffice-qt6-fresh
-      blender
-      principia
-      galaxy-buds-client
+      vulkan-tools          # vulkan-tools (for e.g. vulkaninfo)
+
+      # Software:
+      firefox               # Firefox
+      thunderbird           # Thunderbird
+      libreoffice-qt6-fresh # LibreOffice
+      blender               # Blender
+      galaxy-buds-client    # GalaxyBudsClient (unofficial)
+
+      # Games:
+      principia             # Principia
+      the-powder-toy        # The Powder Toy
+      prismlauncher         # Minecraft
     ];
   };
 
