dfcb7cf466510ac97dfd17aebd685554cbe695a9
griffi-gh <prasol258@gmail.com>
2024-07-25 13:34:53 +0200

    temporarily disable zt because it builds it from source

--- modules/base/services/zerotier.nix
+++ modules/base/services/zerotier.nix
@@ -1,9 +1,9 @@
 { config, lib, pkgs, ... }: {
-  services.zerotierone = {
-    enable = true;
-    port = 9993;
-    joinNetworks = [
-      "9e1948db635b9d3e" # personal
-    ];
-  };
+  # services.zerotierone = {
+  #   enable = true;
+  #   port = 9993;
+  #   joinNetworks = [
+  #     "9e1948db635b9d3e" # personal
+  #   ];
+  # };
 }
