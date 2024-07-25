694b7f5bea1ff8b7de83ff46b6bbde62094e2a94
griffi-gh <prasol258@gmail.com>
2024-07-25 15:25:52 +0200

    add more plasma opts

--- modules/home/plasma.nix
+++ modules/home/plasma.nix
@@ -5,15 +5,32 @@
       lookAndFeel = "org.kde.breezedark.desktop";
       wallpaper = "/home/user/nixos/wallpapers/whatever.png"; #TODO use relative path?
     };
-    configFile = {
-      kwinrc.Desktops.Number = {
-        value = 4;
-        immutable = true;
+    configFile =
+      let
+        immutable = value: { inherit value; immutable = true; };
+      in {
+        # plugins
+        kwinrc.Plugins = {
+          cubeEnabled = immutable true; # cube :3
+        };
+
+        # virtual desktops conf.
+        kwinrc.Desktops = {
+          Number = immutable 4;
+          Rows = immutable 2;
+        };
+
+        # enable font antialiasing
+        # TODO this should be different per-machine :p
+        kdeglobals.General = {
+          XftAntialias = immutable true;
+          XftHintStyle = immutable "hintslight";
+          XftSubPixel = immutable "rgb";
+        };
+
+        # systemsettingsrc.systemsettings_sidebar_mode = {
+        #   HighlightNonDefaultSettings = immutable true;
+        # };
       };
-      kwinrc.Desktops.Rows = {
-        value = 2;
-        immutable = true;
-      };
-    };
   };
 }
