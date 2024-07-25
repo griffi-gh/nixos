65c4a658a9f113c1ff069c610d1812b167143424
griffi-gh <prasol258@gmail.com>
2024-07-25 16:41:43 +0200

    add lshw and pciutils

--- hosts/asus-pc/home/user.nix
+++ hosts/asus-pc/home/user.nix
@@ -7,14 +7,17 @@
       # CLI tools:
       htop
       tree
+      pciutils
       glxinfo
       vulkan-tools          # vulkan-tools (for e.g. vulkaninfo)
+      lshw-gui              # (provides both lshw command and the gui :p)
 
       # Software:
       firefox               # Firefox
       thunderbird           # Thunderbird
       libreoffice-qt6-fresh # LibreOffice
       blender               # Blender
+      # TODO fix galaxy-buds-client somehow (borked and outdated)
       galaxy-buds-client    # GalaxyBudsClient (unofficial)
 
       # Games:
