c8512634b7031d2be79726d76d31c3775cdc0be0
griffi-gh <prasol258@gmail.com>
2024-08-12 11:06:31 +0200

    move stuff for nyow

--- hosts/asus-pc/home/user.nix
+++ hosts/asus-pc/home/user.nix
@@ -1,35 +1,4 @@
 { pkgs, ... }: {
-  home = {
-    stateVersion = "24.11";
-    username = "user";
-    homeDirectory = "/home/user";
-    packages = with pkgs; [
-      # CLI tools:
-      htop
-      tree
-      pciutils
-      glxinfo
-      vulkan-tools          # vulkan-tools (for e.g. vulkaninfo)
-      lshw-gui              # (provides both lshw command and the gui :p)
-
-      # Software:
-      thunderbird           # Thunderbird
-      libreoffice-qt6-fresh # LibreOffice
-      blender               # Blender
-      # TODO fix galaxy-buds-client somehow (borked and outdated)
-      galaxy-buds-client    # GalaxyBudsClient (unofficial)
-
-      # Games:
-      principia             # Principia
-      the-powder-toy        # The Powder Toy
-      prismlauncher         # Minecraft
-    ];
-  };
-
-  programs = {
-    home-manager.enable = true;
-  };
-
   imports = [
     ../../../modules/home.nix
   ];
