ae2bb33df3ed0e69dfc862b4813eda51ea87b8a8
griffi-gh <prasol258@gmail.com>
2024-08-13 20:32:11 +0200

    a

--- modules/base/programs/virtualbox.nix
+++ modules/base/programs/virtualbox.nix
@@ -1,6 +1,7 @@
 { ... }: {
     virtualisation.virtualbox.host = {
         enable = true;
+        # enableKvm = true;
         # enableExtensionPack = true;
     };
     users.extraGroups.vboxusers.members = [ "user" ];
