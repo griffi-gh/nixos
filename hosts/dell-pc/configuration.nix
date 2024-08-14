447118f28f173a82736adc51af330d3a9eb5a9a6
griffi-gh <prasol258@gmail.com>
2024-08-14 16:51:27 +0200

    add modprobe config

--- hosts/dell-pc/configuration.nix
+++ hosts/dell-pc/configuration.nix
@@ -40,6 +40,12 @@ in {
 
   # HACK: wifi
   networking.networkmanager.wifi.powersave = false;
+  boot.extraModprobeConfig = ''
+    options rtw88_core disable_lps_deep=y
+    options rtw88_pci disable_msi=y disable_aspm=y
+    options rtw_core disable_lps_deep=y
+    options rtw_pci disable_msi=y disable_aspm=y
+  '';
 
   # HACK: disable hibernation
   boot.kernelParams = [ "nohibernate" ];
