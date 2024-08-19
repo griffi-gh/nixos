5767fa251614becdf020fe006d75641bcb8545ca
griffi-gh <prasol258@gmail.com>
2024-08-19 20:51:41 +0200

    add gamescope

--- modules/base/programs/steam.nix
+++ modules/base/programs/steam.nix
@@ -1,7 +1,12 @@
 { ... }: {
-  # Steam can only be installed system-wide :<
+  # Steam can only be installed system-wide :<\
+  programs.gamescope = {
+    enable = true;
+    capSysNice = true;
+  };
   programs.steam = {
     enable = true;
+    gamescopeSession.enable = true;
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
