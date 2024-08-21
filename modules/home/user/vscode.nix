f8f6533accf681a0d37eab72565cd745764df244
griffi-gh <prasol258@gmail.com>
2024-08-21 13:51:00 +0200

    add some settings

--- modules/home/user/vscode.nix
+++ modules/home/user/vscode.nix
@@ -1,14 +1,49 @@
 { pkgs, vscode-extensions, ... }: {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscodium;
+    # package = pkgs.vscodium;
     enableUpdateCheck = false;
     enableExtensionUpdateCheck = false;
     mutableExtensionsDir = false;
-    extensions = with vscode-extensions.open-vsx; [
+    extensions = with vscode-extensions.vscode-marketplace; [
+      # Git
+      eamodio.gitlens
+
+      # Rust
       rust-lang.rust-analyzer
+
+      # Copilot:
+      github.copilot
+      github.copilot-chat
+
+      # Generic:
       gruntfuggly.todo-tree
       shardulm94.trailing-spaces
+
+      # Themes:
+      github.github-vscode-theme
     ];
+    userSettings = {
+      # UI:
+      "workbench.colorTheme" = "GitHub Dark Default";
+      "window.titleBarStyle" = "custom";
+      "window.dialogStyle" = "custom";
+
+      # Editing:
+      "editor.linkedEditing" = true;
+
+      # Telemetry:
+      "telemetry.telemetryLevel" = "off";
+      "workbench.enableExperiments" = false;
+      "gitlens.telemetry.enabled" = false;
+
+      # Misc.:
+      "extensions.autoUpdate" = false;
+      "workbench.settings.enableNaturalLanguageSearch" = false;
+    };
   };
+
+  home.packages = with pkgs; [
+    vscode-runner
+  ];
 }
