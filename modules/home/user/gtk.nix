8a1091306e7bed4e7e31ed726a1eb034a7b3a9ab
griffi-gh <prasol258@gmail.com>
2024-08-25 19:57:50 +0200

    use adw-gtk3, except ff and th

--- modules/home/user/gtk.nix
+++ modules/home/user/gtk.nix
@@ -3,8 +3,10 @@
   gtk = {
     enable = true;
     theme = {
-      package = pkgs.kdePackages.breeze-gtk;
-      name = "Breeze-Dark";
+      # package = pkgs.kdePackages.breeze-gtk;
+      # name = "Breeze-Dark";
+      package = pkgs.adw-gtk3;
+      name = "adw-gtk3-dark";
     };
     gtk3 = {
       extraConfig = {
@@ -16,4 +18,8 @@
       extraCss = "@import 'colors.css';";
     };
   };
+
+  # Still, use Breeze-Dark for Firefox and Thunderbird
+  programs.firefox.profiles.default.settings."widget.content.gtk-theme-override" = "Breeze-Dark";
+  programs.thunderbird.profiles.default.settings."widget.content.gtk-theme-override" = "Breeze-Dark";
 }
