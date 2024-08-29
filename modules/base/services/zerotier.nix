839b3772738adfd5fe48e5f4a6f6766b8dcedd71
griffi-gh <prasol258@gmail.com>
2024-08-29 14:20:03 +0200

    switch to tailscale

--- modules/base/services/zerotier.nix
+++ modules/base/services/zerotier.nix
@@ -1,9 +1,15 @@
-{ ... }: {
-  services.zerotierone = {
-    enable = true;
-    port = 9993;
-    joinNetworks = [
-      "9e1948db635b9d3e" # personal
-    ];
-  };
-}
+# { ... }: let
+#   port = 9993;
+# in {
+#   services.zerotierone = {
+#     enable = true;
+#     inherit port;
+#     joinNetworks = [
+#       "9e1948db635b9d3e" # personal
+#     ];
+#   };
+#   networking.firewall = {
+#     allowedTCPPorts = [ port ];
+#     allowedUDPPorts = [ port ];
+#   };
+# }
