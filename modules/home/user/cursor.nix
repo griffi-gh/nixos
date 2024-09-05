58622c1af1280a46f830e696012e79917aa67791
griffi-gh <prasol258@gmail.com>
2024-09-05 12:39:40 +0200

    set plasma cursor

--- modules/home/user/cursor.nix
+++ modules/home/user/cursor.nix
@@ -1,9 +1,17 @@
-{ pkgs, ... }: {
-  home.pointerCursor = {
+{ pkgs, ... }: let
+  cursor = {
     name = "phinger-cursors-dark";
     package = pkgs.phinger-cursors;
     size = 24;
+  };
+in {
+  home.pointerCursor = {
+    inherit (cursor) name package size;
     gtk.enable = true;
     x11.enable = true;
+  };
+  programs.plasma.workspace.cursor = {
+    theme = cursor.name;
+    inherit (cursor) size;
   };
 }
