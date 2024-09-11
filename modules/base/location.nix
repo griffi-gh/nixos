653fe87406bfac8a6414db49dc4463db8bc361e3
griffi-gh <prasol258@gmail.com>
2024-09-11 17:09:34 +0200

    wip

--- modules/base/location.nix
+++ modules/base/location.nix
@@ -1,4 +1,13 @@
 { ... }: {
   location.provider = "geoclue2";
-  services.geoclue2.enable = true;
+  services.geoclue2 = {
+    enable = true;
+    # appConfig = let
+    #   allowed = { isAllowed = true; isSystem = false; };
+    #   allowed-system = { isAllowed = true; isSystem = true; };
+    # in {
+    #   localtimed = allowed-system;
+    #   firefox = allowed;
+    # };
+  };
 }
