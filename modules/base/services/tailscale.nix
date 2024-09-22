c38f9e4badc1dd9254c2097af3ffe27e0c916d1a
griffi-gh <prasol258@gmail.com>
2024-09-22 15:55:02 +0200

    enable client routing features

--- modules/base/services/tailscale.nix
+++ modules/base/services/tailscale.nix
@@ -6,5 +6,8 @@
     enable = true;
     port = 41641;
     openFirewall = true;
+    # Set to "server" or "both" to allow using as exit node (will enable ip forwarding)
+    # Set to "client" or "both" to allow using exit nodes as client (will set reverse path filter to loose)
+    useRoutingFeatures = "client";
   };
 }
