30750ac7354cf48c15d57f3f03fa7f4b9287fba8
griffi-gh <prasol258@gmail.com>
2024-10-15 11:33:18 +0200

    upd vs opt

--- modules/home/user/vscode/vs-cpp.nix
+++ modules/home/user/vscode/vs-cpp.nix
@@ -4,12 +4,26 @@
       let from-nixpkgs = pkgs.vscode-extensions; in
       with vscode-extensions.vscode-marketplace;
     [
+      # Cmake
+      twxs.cmake
       ms-vscode.cmake-tools
+
+      # Ninja syntax highlighting
+      surajbarkale.ninja
+
+      # C/C++ support
       from-nixpkgs.ms-vscode.cpptools
       llvm-vs-code-extensions.vscode-clangd
     ];
     userSettings = {
       "C_Cpp.intelliSenseEngine" = "disabled"; # Use clangd's intellisense engine
+      # "cmake.options.statusBarVisibility" = "compact";
+      "cmake.showOptionsMovedNotification" = false;
+      "cmake.options.statusBarVisibility" = "icon";
+      "cmake.pinnedCommands" = [
+        "workbench.action.tasks.configureTaskRunner"
+        "workbench.action.tasks.runTask"
+      ];
     };
   };
 }
