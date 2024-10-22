f6b326f20c8d5d629acf5131d77aa32e066a9eff
griffi-gh <prasol258@gmail.com>
2024-10-22 19:41:21 +0200

    fix splash

--- modules/home/user/plasma.nix
+++ modules/home/user/plasma.nix
@@ -69,4 +69,12 @@ in {
   # home.activation."kwin_reconfigure" = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
   #   qdbus org.kde.KWin /KWin reconfigure
   # '';
+
+  home.file."/home/user/.config/systemd/user/plasma-ksplash-ready.service.d/override.conf" = {
+    # fix bug where splash screen doesn't disappear
+    text = ''
+      [Service]
+      ExecStart=/etc/profiles/per-user/user/bin/dbus-send --session --reply-timeout=1 --type=method_call --dest=org.kde.KSplash /KSplash org.kde.KSplash.setStage string:ready
+    '';
+  };
 }
