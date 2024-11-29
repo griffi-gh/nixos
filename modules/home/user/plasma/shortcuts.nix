bf9e2782f6e23e020d344f8f7dc5f1023db78472
griffi-gh <prasol258@gmail.com>
2024-11-29 18:19:55 +0100

    disable plasma-manager shortcuts

--- modules/home/user/plasma/shortcuts.nix
+++ modules/home/user/plasma/shortcuts.nix
@@ -1,9 +1,10 @@
 { ... }: {
-  programs.plasma.shortcuts = {
-    # yakuake.toggle-window-state = "Calculator,F12,Open/Retract Yakuake";
-    "services/org.kde.spectacle.desktop".RectangularRegionScreenShot = [
-        "Meta+Shift+Print"
-        "Meta+`"
-    ];
-  };
+  # XXX: borked
+  # programs.plasma.shortcuts = {
+  #   # yakuake.toggle-window-state = "Calculator,F12,Open/Retract Yakuake";
+  #   "services/org.kde.spectacle.desktop".RectangularRegionScreenShot = [
+  #       "Meta+Shift+Print"
+  #       "Meta+`"
+  #   ];
+  # };
 }
