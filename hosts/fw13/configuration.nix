2a76a20bf6daa29a8f5f2f36c1138fa27a41cdf4
griffi-gh <prasol258@gmail.com>
2025-01-02 22:13:41 +0100

    add resume

--- hosts/fw13/configuration.nix
+++ hosts/fw13/configuration.nix
@@ -6,8 +6,14 @@ in {
   ];
 
   system.stateVersion = "25.05";
-
   networking.hostName = hostname;
+
+  # hardware.framework.laptop13.audioEnhancement = true;
+
+  boot.kernelParams = [
+    "resume=UUID=4f0b5893-4d99-4dbc-9ba0-1ab0ac6c3cfc"
+    # "i915.enable_psr=1"
+  ];
 
   fileSystems = let
     btrfsOptions = [ "ssd" "noatime" "nodiscard" "compress=zstd:1" ];
