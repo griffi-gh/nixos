3d55bdc8a7e9f01f4e431e604fe43ed6c1056b28
griffi-gh <prasol258@gmail.com>
2025-01-02 17:14:50 +0100

    set icon size to 16

--- modules/home/user/plasma/dolphin.nix
+++ modules/home/user/plasma/dolphin.nix
@@ -1,13 +1,19 @@
 { ... }: {
-  programs.plasma.configFile.dolphinrc = {
+  programs.plasma.configFile.dolphinrc = let
+    sidebarIconSize = 16;
+  in {
     General = {
       GlobalViewProps = false;
       ConfirmClosingMultipleTabs = false;
       RememberOpenedTabs = false;
       ShowFullPath = true;
     };
+    "KFileDialog Settings" = {
+      "Places Icons Auto-resize" = false;
+      "Places Icons Static Size" = sidebarIconSize;
+    }
     PlacesPanel = {
-      IconSize = -1;
+      IconSize = sidebarIconSize;
     };
   };
 }
