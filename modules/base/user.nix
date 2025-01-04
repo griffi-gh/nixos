573b796740823a6acbff5e3901832952404fd16f
griffi-gh <prasol258@gmail.com>
2025-01-04 16:44:39 +0100

    trust fw13 key

--- modules/base/user.nix
+++ modules/base/user.nix
@@ -18,6 +18,8 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID7VB6Mohz6Zo0ygBpdhuKinxELomEXUyQzdyLd5u00M prasol258@gmail.com"
       # dell-pc
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKeD5o9/2cpH7HaP4PldZYKgJnltFX9XlC5bvcxpOivq user@dell-pc"
+      # fw13
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB23Z5khaSPYhYX1AZFZIVKwjXaDZbGQFVxzPPWa6f4r user@fw13"
     ];
   };
 }
