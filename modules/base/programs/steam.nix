48888ee8047665b4e873124518dcabb4cea263ae
griffi-gh <prasol258@gmail.com>
2025-01-15 18:21:10 +0100

    add proton-ge-bin

--- modules/base/programs/steam.nix
+++ modules/base/programs/steam.nix
@@ -5,6 +5,9 @@
     remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
     dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
     localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    extraCompatPackages = [
+      pkgs.proton-ge-bin
+    ];
   };
   environment.systemPackages = with pkgs; [
     protonup-qt
