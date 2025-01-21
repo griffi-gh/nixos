75578b656cf8d723a47a80c484c041a7431bbab2
griffi-gh <prasol258@gmail.com>
2025-01-21 12:34:30 +0100

    fix override

--- pkgs/ciscoPacketTracer8-wrapped/package.nix
+++ pkgs/ciscoPacketTracer8-wrapped/package.nix
@@ -2,33 +2,25 @@
   fetchurl,
   symlinkJoin,
   makeWrapper,
-  # callPackage,
   ciscoPacketTracer8,
 }:
+let
+  ciscoPacketTracer8' = ciscoPacketTracer8.override {
+    version = "8.2.2";
+    packetTracerSource = fetchurl {
+      url = "https://files-cdn.girl.pp.ua/cisco/Packet_Tracer822_amd64_signed.deb";
+      sha256 = "6cd2b8891df92d2cad8b6fdc47480fc089de085c4f3fe95eb80d5450a2a7f72d";
+    };
+  };
+in
 symlinkJoin {
   name = "ciscoPacketTracer8-wrapped";
-  paths = let
-    # ciscoPacketTracer8' = (callPackage ./ciscoPacketTracer8.nix {
-    #   requireFile = _: fetchurl {
-    #     url = "https://files-cf.beeg.pp.ua/cisco/Packet_Tracer822_amd64_signed.deb";
-    #     sha256 = "6cd2b8891df92d2cad8b6fdc47480fc089de085c4f3fe95eb80d5450a2a7f72d";
-    #   };
-    # });
-    ciscoPacketTracer8' = ciscoPacketTracer8.override {
-      version = "8.2.2";
-      __functionArgs = {
-        packetTracerSource = fetchurl {
-          url = "https://files-cdn.girl.pp.ua/cisco/Packet_Tracer822_amd64_signed.deb";
-          sha256 = "6cd2b8891df92d2cad8b6fdc47480fc089de085c4f3fe95eb80d5450a2a7f72d";
-        };
-      };
-    };
-  in [
+  paths = [
     ciscoPacketTracer8'
   ];
   nativeBuildInputs = [ makeWrapper ];
   postBuild = ''
-    cp $out/bin/packettracer8 $out/bin/packettracer8-wl
+    ln -s $out/bin/packettracer8 $out/bin/packettracer8-wl
     wrapProgram $out/bin/packettracer8 \
       --set GDK_BACKEND "x11" \
       --set XDG_CURRENT_DESKTOP "GNOME";
