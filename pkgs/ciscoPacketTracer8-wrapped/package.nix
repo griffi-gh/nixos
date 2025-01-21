7a95cf0d0f6535750f42b125e3541c07157df83b
griffi-gh <prasol258@gmail.com>
2025-01-21 11:59:33 +0100

    switch back to nixpkgs pt8

--- pkgs/ciscoPacketTracer8-wrapped/package.nix
+++ pkgs/ciscoPacketTracer8-wrapped/package.nix
@@ -2,14 +2,21 @@
   fetchurl,
   symlinkJoin,
   makeWrapper,
-  callPackage,
+  # callPackage,
+  ciscoPacketTracer8,
 }:
 symlinkJoin {
   name = "ciscoPacketTracer8-wrapped";
   paths = let
-    ciscoPacketTracer8' = (callPackage ./ciscoPacketTracer8.nix {
-      requireFile = _: fetchurl {
-        url = "https://files-cf.beeg.pp.ua/cisco/Packet_Tracer822_amd64_signed.deb";
+    # ciscoPacketTracer8' = (callPackage ./ciscoPacketTracer8.nix {
+    #   requireFile = _: fetchurl {
+    #     url = "https://files-cf.beeg.pp.ua/cisco/Packet_Tracer822_amd64_signed.deb";
+    #     sha256 = "6cd2b8891df92d2cad8b6fdc47480fc089de085c4f3fe95eb80d5450a2a7f72d";
+    #   };
+    # });
+    ciscoPacketTracer8' = ciscoPacketTracer8.overrideAttrs(_: {
+      packetTracerSource = fetchurl {
+        url = "https://files-cdn.girl.pp.ua/cisco/Packet_Tracer822_amd64_signed.deb";
         sha256 = "6cd2b8891df92d2cad8b6fdc47480fc089de085c4f3fe95eb80d5450a2a7f72d";
       };
     });
