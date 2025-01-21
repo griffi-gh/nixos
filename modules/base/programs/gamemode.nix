f4bdd39066b75f262922038e74a1ef5aa9632c92
griffi-gh <prasol258@gmail.com>
2025-01-21 17:45:42 +0100

    disable gamemode config

--- modules/base/programs/gamemode.nix
+++ modules/base/programs/gamemode.nix
@@ -2,19 +2,19 @@
   # Gamemoderun
   programs.gamemode = {
     enable = true;
-    settings = {
-      general = {
-        renice = 10;
-        desiredgov = "performance"; # governor to use while gaming
-        igpu_desiredgov = "performance"; # governor when igpu is under load
-        igpu_power_threshold = -1; # mayb just set it to sth like 0.9 to avoid overheating?
-      };
-      gpu = {
-        apply_gpu_optimisations = "accept-responsibility";
-        gpu_device = 1; # <- this is machine-specific (usually 0, but this one's weird)
-        amd_performance_level = "high"; # force high clocks
-      };
-    };
+    # settings = {
+    #   general = {
+    #     renice = 10;
+    #     desiredgov = "performance"; # governor to use while gaming
+    #     igpu_desiredgov = "performance"; # governor when igpu is under load
+    #     igpu_power_threshold = -1; # mayb just set it to sth like 0.9 to avoid overheating?
+    #   };
+    #   gpu = {
+    #     apply_gpu_optimisations = "accept-responsibility";
+    #     gpu_device = 1; # <- this is machine-specific (usually 0, but this one's weird)
+    #     amd_performance_level = "high"; # force high clocks
+    #   };
+    # };
   };
   # Breaks Steam games:
   # environment.sessionVariables = {
