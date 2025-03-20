4e5098e95a0fbd7b80a6dd70cb9d22d4c8274ce4
griffi-gh <prasol258@gmail.com>
2025-03-20 02:12:53 +0100

    ass assignments for kde and nix

--- modules/base/system76-scheduler.nix
+++ modules/base/system76-scheduler.nix
@@ -1,8 +1,31 @@
 { ... }: {
   # enable system76 scheduler
-  # TODO: prioritize plasmashell, kwin and KRunner
   services.system76-scheduler = {
     enable = true;
     useStockConfig = true;
+    assignments = {
+      package-manager-nix = {
+        nice = 15;
+        class = "batch";
+        ioClass = "idle";
+        matchers = [
+          "nix"
+          "nix-daemon"
+          "nixos-rebuild"
+        ];
+      };
+      desktop-environment-kde = {
+        nice = -3;
+        ioClass = "best-effort";
+        ioPrio = 0;
+        matchers = [
+          "plasmashell"
+          "kwin_x11"
+          "kwin_wayland"
+          "kwin_wayland_wrapper"
+          "krunner"
+        ];
+      };
+    };
   };
 }
