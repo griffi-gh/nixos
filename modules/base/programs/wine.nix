6377a30cbfc4817a1698198f697f1a38d333c6f1
griffi-gh <prasol258@gmail.com>
2025-04-01 12:18:26 +0200

    add wine64 alias

--- modules/base/programs/wine.nix
+++ modules/base/programs/wine.nix
@@ -1,5 +1,9 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     wineWowPackages.staging
+    (writeShellScriptBin "wine64" ''
+      #!/usr/bin/env bash
+      exec "${wineWowPackages.staging}/bin/wine" "$@"
+    '')
   ];
 }
