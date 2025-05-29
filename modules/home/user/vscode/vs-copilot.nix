c167ed91ce29a1d5a36d38a15b8c669940161af0
griffi-gh <prasol258@gmail.com>
2025-05-29 20:59:41 +0200

    vscode: add speech module for copilot

--- modules/home/user/vscode/vs-copilot.nix
+++ modules/home/user/vscode/vs-copilot.nix
@@ -1,8 +1,12 @@
 { pkgs, ...}: {
   programs.vscode.profiles.default = {
-    extensions = with pkgs.vscode-extensions; [
-      github.copilot
-      github.copilot-chat
+    extensions =
+      let from-nixpkgs = pkgs.vscode-extensions; in
+      with pkgs.vscode-marketplace;
+    [
+      from-nixpkgs.github.copilot
+      from-nixpkgs.github.copilot-chat
+      ms-vscode.vscode-speech
     ];
     userSettings = {
       # "chat.commandCenter.enabled" = false;
