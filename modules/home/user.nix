6a8e49d7fa8527eccd34b1fd8e1924c51ad6745c
griffi-gh <prasol258@gmail.com>
2025-06-16 18:01:28 +0200

    add ani-cli to user.nix

--- modules/home/user.nix
+++ modules/home/user.nix
@@ -34,7 +34,8 @@
       btop         # (system monitor) htop but even more autistic :3
       ffmpeg       # ffmpeg
       imagemagick  # ImageMagick (default version)
-      http-server   # http-server (node.js)
+      http-server  # http-server (node.js)
+      ani-cli      # anime cli
 
       amdgpu_top # (MAYBE move to hw modules?)
 
