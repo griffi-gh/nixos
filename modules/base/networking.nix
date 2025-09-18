c0f7021d75b10c003c6e92700e6eb454e8c6efd8
griffi-gh <prasol258@gmail.com>
2025-09-18 12:28:42 +0200

    Update networking.nix

--- modules/base/networking.nix
+++ modules/base/networking.nix
@@ -1,41 +1,54 @@
-{ ... }: {
+{ ... }:
+{
   # networking :3
-  networking = {
-    networkmanager = {
-      enable = true;
-      # wifi.backend = "iwd";
-      # issues:
-      # - makes it forget wifi network is protected
-      # - wpa3 is borked
-      # - no hotspot
-    };
-    # wireless.iwd.enable = true;
-
-    nftables.enable = true;
-    firewall = {
-      enable = true;
-      allowedTCPPorts = [
-        22    # ssh
-        3389  # rdp
-        5900  # vnc
-        1714 1764 # kde connect
-        18885 18886 # torrent
-        61208 61209 # glances server
-        # 2234  # soulseek/nicotine+
-      ];
-      allowedUDPPorts = [
-        3389  # rdp
-        5900  # vnc
-        1714 1764 # kde connect
-        18886 # torrent
-        34197 # factorio
-      ];
-    };
-
-    # proxy.default = "http://user:password@proxy:port/";
-    # proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+  networking.networkmanager = {
+    enable = true;
+    dns = "systemd-resolved";
+    # wifi.backend = "iwd";
   };
 
-  # Unnecesary on single-user systems, slows down boot, causes rebuilds to fail
+  # dns
+  networking.resolvconf.enable = false;
+  services.resolved = {
+    enable = true;
+    dnssec = "allow-downgrade";
+    dnsovertls = "opportunistic";
+    fallbackDns = [
+      # opendns sandbox
+      "2620:0:ccd::2"
+      "2620:0:ccc::2"
+      "208.67.220.2"
+      "208.67.222.2"
+      # tailscale
+      # "100.100.100.100"
+    ];
+  };
+
+  # firewall
+  networking.nftables.enable = true;
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [
+      22 # ssh
+      3389 # rdp
+      5900 # vnc
+      1714
+      1764 # kde connect
+      18885
+      18886 # torrent
+      61208
+      61209 # glances server
+    ];
+    allowedUDPPorts = [
+      3389 # rdp
+      5900 # vnc
+      1714
+      1764 # kde connect
+      18886 # torrent
+      34197 # factorio
+    ];
+  };
+
+  # unnecessary/causes rebuilds to fail
   systemd.services.NetworkManager-wait-online.enable = false;
 }
