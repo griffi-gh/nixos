533e2df9770ad43f3a770f71ce4ac80d2b32dfac
griffi-gh <prasol258@gmail.com>
2025-09-19 22:02:30 +0200

    add gutenprint driver

--- modules/base/services/printing.nix
+++ modules/base/services/printing.nix
@@ -1,8 +1,13 @@
-{ ... }: {
+{ pkgs, ... }:
+{
   # Enable CUPS to print documents.
   services.printing = {
     enable = true;
     stateless = true;
     startWhenNeeded = true;
+    drivers = with pkgs; [
+      gutenprint
+    ];
+    # canon-cups-ufr2, cnijfilter?
   };
 }
