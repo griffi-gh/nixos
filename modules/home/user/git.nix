5b17bec3dab3dde13d49cd2f18e06a692fddae5f
griffi-gh <prasol258@gmail.com>
2025-09-21 19:28:43 +0200

    gitignore .directory

--- modules/home/user/git.nix
+++ modules/home/user/git.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   programs.git = {
     enable = true;
     package = pkgs.gitFull;
@@ -19,6 +20,7 @@
       };
     };
     ignores = [
+      ".directory"
       ".direnv"
       "*.kate-swp"
     ];
